6ceb8fde25b4ef339a9ef52d716bef87f98e3493
shobika.palani <shobika.palani@syncfusion.com>
2018-11-15 17:14:19 +0530

    How to serialize the filtered values based on FilterMode of the column

---
+++ WPF/Helpers/CustomSerializationController.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridDemo
+{
+    public class CustomSerializationController : SerializationController
+    {
+        public CustomSerializationController(SfDataGrid dataGrid) : base(dataGrid)
+        {
+
+        }
+        protected override SerializableFilterSettings StoreFilterPredicates(SfDataGrid dataGrid, SerializationOptions serializeOptions)
+        {
+            SerializableFilterSettings filterSettings = new SerializableFilterSettings();
+            if (!serializeOptions.SerializeFiltering)
+                return filterSettings;
+
+            foreach (var column in dataGrid.Columns.Where(c => c.FilterPredicates.Count > 0 && c.FilteredFrom== FilteredFrom.AdvancedFilter))
+            {
+                SerializableFilterSetting filterSetting = new SerializableFilterSetting() { ColumnName = column.MappingName, Filter = new ObservableCollection<SerializableFilter>() };
+                foreach (var filterPredicate in column.FilterPredicates)
+                {
+                    SerializableFilter filter = new SerializableFilter() { FilterBehavior = filterPredicate.FilterBehavior, FilterType = filterPredicate.FilterType, FilterValue = filterPredicate.FilterValue, IsCaseSensitive = filterPredicate.IsCaseSensitive, PredicateType = filterPredicate.PredicateType };
+                    filterSetting.Filter.Add(filter);
+                }
+                filterSettings.Add(filterSetting);
+            }
+            return filterSettings;
+        }
+    }
+}
---
+++ WPF/Helpers/SfDataGridBehavior.cs
@@ -0,0 +1,20 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interactivity;
+
+namespace SfDataGridDemo
+{
+    public class SfDataGridBehavior : Behavior<SfDataGrid>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.SerializationController = new CustomSerializationController(this.AssociatedObject);
+        }
+    }
+}
